9672a90e73e31f3c02a8abd209db9c84e28a4dcb
APPX01 <bhushan.patil@appxtechnologies.com>
2020-02-15 15:12:52 +0530

    final commit

--- src/component/HomePageModal.js
+++ src/component/HomePageModal.js
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+
 const HomePageModal = props => {
     return ReactDOM.createPortal(
         <div onClick={props.onDismiss}
-         className='ui dimmer modals visible active'>
+         className='ui dimmer modals visible active' >
         <div onClick={(e) => e.stopPropagation()} 
-        className='ui standard modal visible active'>
-        <div className='header'>{props.title}</div>
-        <div className='content'>
+        className='ui standard modal visible active' >
+        <div className='header' style={{ backgroundColor: '#28B9B5', color:'white'}}>{props.title}</div>
+        <div className='content' style={{ backgroundColor: '#28B9B5', color:'white', fontSize: 'large'}}>
           {props.content}
         </div>
-        <div className='actions'>
+        <div className='actions' style={{ backgroundColor: '#28B9B5',  }}>
         {props.actions}
         </div>
         </div>
